9554df251d2cf32eec4466a6e0498768c6da19e3
Nikolay Vinogradov <NikolayVinogradov15@yandex.ru>
2021-02-27 18:38:08 +0300

    added getting types and brands

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,16 +12,45 @@ import { ShopService } from './shop.service';
 export class ShopComponent implements OnInit {
 
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts()
+    this.getBrands()
+    this.getTypes()
+  }
+
+  getProducts(){
     this.shopService.getProducts().subscribe(response =>{
       this.products = response.data;
-      console.log("Product uploaded")
-    }, error =>{
+      console.log("Products uploaded")
+    }, 
+      error =>{
       console.log(error)
-    })
+    });
+  }
+
+  getBrands(){
+    return this.shopService.getBrands().subscribe(responce =>{
+      this.brands = responce;
+      console.log("Brands uploaded")
+    }
+      ),
+      error =>
+      console.log(error);
+  }
+
+  getTypes(){
+    return this.shopService.getTypes().subscribe(responce =>{
+      this.types = responce;
+      console.log("Types uploaded")
+    }
+      ),
+      error =>
+      console.log(error);
   }
 
 }
