2de5d398cb6e24f5873b608126662420c98a2ef1
Nikolay Vinogradov <NikolayVinogradov15@yandex.ru>
2021-03-27 22:42:36 +0300

    added loading service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module'
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingIntercemtor } from './core/interceptors/loading.interceptors';
 
 @NgModule({
   declarations: [
@@ -21,9 +21,11 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     HttpClientModule,
     CoreModule,
     HomeModule,
-    
+    NgxSpinnerModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+              {provide: HTTP_INTERCEPTORS, useClass: LoadingIntercemtor, multi: true}],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
