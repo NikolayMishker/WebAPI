98c6e5ca4d5f7d7e56e67b825791c83710126f05
Nikolay Vinogradov <NikolayVinogradov15@yandex.ru>
2021-06-06 16:05:11 +0300

    Create PaymentsController and updated customerbasketDto

---
+++ WebAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    public class PaymentsController : BaseApiController
+    {
+        private readonly IPaymentService paymentService;
+        public PaymentsController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return await paymentService.CreateOrUpdatePaymentIntent(basketId);
+        }
+    }
+}
